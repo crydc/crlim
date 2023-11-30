230045932cb26b2549bf81b103e5cd4b64d35418
crydc <97872790+crydc@users.noreply.github.com>
2023-11-30 21:13:34 +0800

    Add files via upload

---
+++ windows.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Welcome to My flask_app by Spark_ RAG!
+
+REM 运行命令
+echo Current Date and Time:
+date /t
+time /t
+pip install -r requirements.txt
+
+REM 执行 Python 脚本
+python main.py
+
